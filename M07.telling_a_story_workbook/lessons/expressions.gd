d9e76625b172b33630bc51c85e79c9a4d72e9d63
Ryan Huang <ryhuang3@s.sfusd.edu>
2025-11-03 15:45:36 -0800

    M7L8

--- M07.telling_a_story_workbook/lessons/expressions.gd
+++ M07.telling_a_story_workbook/lessons/expressions.gd
@@ -1,11 +1,41 @@
 extends Control
 
+@onready var body: TextureRect = %Body
+@onready var expression: TextureRect = %Expression
+@onready var button_sophia: Button = %ButtonSophia
+@onready var button_pink: Button = %ButtonPink
+@onready var button_regular: Button = %ButtonRegular
+@onready var button_sad: Button = %ButtonSad
+@onready var button_happy: Button = %ButtonHappy
 
-# Called when the node enters the scene tree for the first time.
+var bodies := {
+	"sophia": preload("res://assets/sophia.png"),
+	"pink": preload("res://assets/pink.png")
+}
+
+var expressions := {
+	"happy": preload("res://assets/emotion_happy.png"),
+	"regular": preload("res://assets/emotion_regular.png"),
+	"sad": preload("res://assets/emotion_sad.png"),
+}
+
 func _ready() -> void:
-	pass # Replace with function body.
+	body.texture = bodies["pink"]
+	expression.texture = expressions["happy"]
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	button_sophia.pressed.connect(func() -> void:
+		body.texture = bodies["sophia"]
+	)
+	button_pink.pressed.connect(func() -> void:
+		body.texture = bodies["pink"]
+	)
+	button_regular.pressed.connect(func() -> void:
+		expression.texture = expressions["regular"]
+	)
+	button_sad.pressed.connect(func() -> void:
+		expression.texture = expressions["sad"]
+	)
+	button_happy.pressed.connect(func() -> void:
+		expression.texture = expressions["happy"]
+	)
+	
