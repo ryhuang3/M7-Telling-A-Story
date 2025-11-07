c9bf7d4c852e134bc1bfae8283de2c396e8271ae
Ryan Huang <ryhuang3@s.sfusd.edu>
2025-11-07 14:35:08 -0800

    M7L12

--- M07.telling_a_story_workbook/lessons/dialogue.gd
+++ M07.telling_a_story_workbook/lessons/dialogue.gd
@@ -11,28 +11,59 @@ var expressions := {
 	"regular": preload ("res://assets/emotion_regular.png"),
 	"sad": preload ("res://assets/emotion_sad.png"),
 }
+var bodies := {
+	"sophia": preload ("res://assets/sophia.png"),
+	"pink": preload ("res://assets/pink.png")
+}
 
 var dialogue_items: Array[Dictionary] = [
-	{"expression": expressions["happy"],
+	{"expression": expressions["regular"],
 	"text": "Morning!",
+	"character":bodies["pink"],
 	},
-	{"expression": expressions["sad"],
-	"text": "It's a bit crowded in here eh?",
+	{"expression": expressions["regular"],
+	"text": "It's a bit crowded in here...",
+	"character":bodies["sophia"],
 	},
 	{"expression": expressions["sad"],
 	"text": "And I'm a bit claustrophobic.",
+	"character":bodies["sophia"],
+	},
+	{"expression": expressions["happy"],
+	"text": "Well too bad too sad.",
+	"character":bodies["pink"],
 	},
 	{"expression": expressions["regular"],
+	"character":bodies["sophia"],
 	"text": "Let me out please!",
+	},
+	{"expression": expressions["happy"],
+	"text": "How 'bout no",
+	"character":bodies["pink"],
 	},
 	{"expression": expressions["sad"],
 	"text": "What did I do to deserve this?",
+	"character":bodies["sophia"],
+	},
+	{"expression": expressions["happy"],
+	"text": "You were never a true friend",
+	"character":bodies["pink"],
 	},
 	{"expression": expressions["sad"],
 	"text": "PLEASE!",
+	"character":bodies["sophia"],
+	},
+	{"expression": expressions["sad"],
+	"text": "I'LL DO ANYTHING!",
+	"character":bodies["sophia"],
+	},
+	{"expression": expressions["happy"],
+	"text": "HAHAHAHA, ROT HERE FOR ALL OF ETERNITY",
+	"character":bodies["pink"],
 	},
 	{"expression": expressions["sad"],
 	"text": "NOOOOOOO",
+	"character":bodies["sophia"],
 	},
 ]
 var current_item_index := 0
@@ -45,15 +76,17 @@ func show_text() -> void:
 	var current_item := dialogue_items[current_item_index]
 	rich_text_label.text = current_item["text"]
 	expression.texture = current_item["expression"]
+	body.texture = current_item["character"]
 	rich_text_label.visible_ratio = 0.0
 	var tween := create_tween()
-	var text_appearing_duration := 1.2
+	var text_appearing_duration: float = current_item["text"].length() / 30.0
 	tween.tween_property(rich_text_label, "visible_ratio", 1.0, text_appearing_duration)
 	var sound_max_offset := audio_stream_player.stream.get_length() - text_appearing_duration
 	var sound_start_position := randf() * sound_max_offset
 	audio_stream_player.play(sound_start_position)
 	tween.finished.connect(audio_stream_player.stop)
 	slide_in()
+
 func advance() -> void:
 	current_item_index += 1
 
